bbe2f94677d0f5ad95432368ddb5c2aa408ed56a
Crazi12345 <patrickbjerre@outlook.dk>
2023-10-22 12:39:12 +0000

    read credentials

--- Data-Sanitation/cleanse_engine/src/main.rs
+++ Data-Sanitation/cleanse_engine/src/main.rs
@@ -1,3 +1,24 @@
+use std::fs;                                                                                          
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct Creds {
+    auth_uri: String,
+    client_id: String,
+    client_secret: String,
+    redirect_uris: [String;1],
+    token_uri: String,
+    project_id: String
+}
+fn read_config() -> Creds {
+    let data = fs::read_to_string("credentials.json")
+        .expect("Unable to read file");
+    let creds: Creds = serde_json::from_str(&data)
+        .expect("Unable to parse JSON");
+    creds
+}
+
 fn main() {
-    println!("Hello, world!");
+   let test:Creds = read_config();
+   println!("{}",test.client_id);
 }
