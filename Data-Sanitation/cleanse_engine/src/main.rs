61387c033c536bf5d9af67c3bd5f5a137498ba3d
Crazi12345 <patrickbjerre@outlook.dk>
2023-10-23 10:37:24 +0000

    get all events on calendar

--- Data-Sanitation/cleanse_engine/src/main.rs
+++ Data-Sanitation/cleanse_engine/src/main.rs
@@ -1,14 +1,11 @@
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use reqwest;
 use std::fs;                                                                                          
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
 struct Creds {
-    auth_uri: String,
-    client_id: String,
-    client_secret: String,
-    redirect_uris: [String;1],
-    token_uri: String,
-    project_id: String
+    api_key: String
 }
 fn read_config() -> Creds {
     let data = fs::read_to_string("credentials.json")
@@ -17,8 +14,30 @@ fn read_config() -> Creds {
         .expect("Unable to parse JSON");
     creds
 }
+async fn get_calendar_events() -> reqwest::Result<String>{
+    let creds = read_config();
+    let api_key = creds.api_key;
+    let calendar_id = "4cd37135fc423abf9738ad1535bb9c8691e74cbdaa6973225cde3225e97a4085@group.calendar.google.com";
+    let url = format!("https://www.googleapis.com/calendar/v3/calendars/{}/events?key={}",calendar_id,api_key);
+    let response = reqwest::get(&url).await?.text().await?;
+    println!("{}",response);
+    Ok(response)
+}
 
-fn main() {
-   let test:Creds = read_config();
-   println!("{}",test.client_id);
+async fn events() -> impl Responder {
+    match get_calendar_events().await {
+        Ok(data) => HttpResponse::Ok().body(data),
+        Err(_) => HttpResponse::InternalServerError().finish(),
+    }
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/events", web::get().to(events))
+    })
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await
 }
